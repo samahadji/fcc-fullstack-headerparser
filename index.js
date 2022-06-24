6668ccc50214a3b64124f28b4000211068b37e72
Samson Ahadji <samahadji@gmail.com>
2022-06-24 15:32:04 +0000

    Fix the request reading

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami/", function (req, res) {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.header("accept-language"),
+    "software": req.headers["user-agent"]
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
